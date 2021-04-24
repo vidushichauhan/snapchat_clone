466b5afb20e83e181b6013813f8a5b1a08ae9e73
vidushichauhan <vidushic0@gmail.com>
2021-04-24 18:10:13 +0530

    webcamcapture edit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 
 import './App.css';
+import WebcamCapture from './WebcamCapture';
 
 function App() {
   return (
     <div className="App">
     <h1>SNAPCHAT</h1>
+    <WebcamCapture/>
     </div>
   );
 }
